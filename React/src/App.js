59a4edb1b14e69826d500c3b0f2ce0497f38bb7d
Jefferstarck <jeffersonriapirac@gmail.com>
2023-07-31 18:53:58 -0500

    screen login users

--- React/src/App.js
+++ React/src/App.js
@@ -1,28 +1,50 @@
-import logo from './logo.svg';
-import usuarios from './usuarios.png';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    // Aquí puedes agregar la lógica para autenticar al usuario con tu backend o API.
+    // Por ahora, simplemente mostraremos un mensaje de error si los campos están vacíos.
+
+    if (username.trim() === '' || password === '') {
+      setError('Por favor, ingresa tu nombre de usuario y contraseña.');
+    } else {
+      setError('');
+      // Aquí puedes realizar la lógica de autenticación o redireccionar a la siguiente página.
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={usuarios} className="Usuarios" alt="usuarios"/>
-        <p> This is my change, the image that is above... </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.cualquier maricada
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>ESTO ES UN NUEVO CAMBIO</p>
-        <p>ESTO ES OTRO NUEVO CAMBIO.</p>
-      </header>
+      <h1>Iniciar sesión</h1>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label>Nombre de usuario:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Ingresa tu nombre de usuario"
+          />
+        </div>
+        <div className="form-group">
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Ingresa tu contraseña"
+          />
+        </div>
+        {error && <div className="error">{error}</div>}
+        <button type="submit">Iniciar sesión</button>
+      </form>
     </div>
   );
 }
